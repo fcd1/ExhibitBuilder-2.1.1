554fd875e4e32d3cb1aa5390b2950900173df300
Frederic Duby <fcd1@columbia.edu>
2014-11-24 14:30:35 -0500

    Initial commit new layouts: large images centered, text above or below

---
+++ views/shared/exhibit_layouts/image-list-center-text-bottom/layout.php
@@ -0,0 +1,36 @@
+<div class="image-list-center-text-bottom">
+  <?php
+    for ($i = 1; $i <= 8; $i++):
+
+      $text = exhibit_builder_page_text($i,$exhibitPage);
+      $attachment = exhibit_builder_page_attachment($i,0,$exhibitPage);
+
+      if ($text || $attachment):
+
+        echo '<div class="image-text-group">';
+
+        if ($attachment):
+
+          echo '<div class="exhibit-item">';
+          echo cul_exhibit_builder_attachment_markup($attachment, 
+						     array('imageSize' => 'fullsize'), 
+						     array('class' => 'permalink'));
+          echo '</div>';
+
+        endif;
+
+        if ($text):
+
+          echo '<div class="exhibit-text">';
+          echo $text;
+          echo '</div>';
+
+        endif;
+
+        echo '</div>';
+
+      endif;
+
+    endfor;
+  ?>
+</div>
